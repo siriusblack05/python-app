e5d755880eb0f28fa55400b43d8b566ee3f68633
siriusblack <huseyin05topal@gmail.com>
2021-11-23 21:19:25 +0300

    leap year.py fixed

--- leap-year.py
+++ leap-year.py
@@ -1,5 +1,16 @@
 year=int(input("Enter year to be checked:"))
 if(year%4==0 and year%100!=0 or year%400==0):
+
+    [Oyear=int(input("Enter year to be checked:"))
+if(year%4==0 and year%100!=0 or year%400==0):
+
+    ^[[O
+    print("THIS IS JUST A LEAP YEAR!)
+else:
+    print("The year isn't a leap year!)
+
+
+          hello
    
     print("THIS IS JUST A LEAP YEAR!)
 else:
